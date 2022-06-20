9590bacffdd1c78897f9bd2c31f08e12c51c7727
Moisés Alarcón <78032561+MoisesAlarcon@users.noreply.github.com>
2022-06-20 15:29:21 +0200

    Create Manual-exponent.js

---
+++ Manual-exponent.js
@@ -0,0 +1,11 @@
+const  toThePowerOf  =  ( num ,  exp )  =>  {
+   elementos  const =  Array ( exp ) . llenar ( num ) ;
+  const  reductor  =  ( acumulador ,  valor actual )  =>  acumulador  *  valor actual ;
+  devolver  artículos . reducir ( reductor ) ;
+} ;
+
+aElPoderDe ( 2 ,  3 ) ;  //
+alPoderDe ( 3 ,  3 ) ;  //
+alPoderDe ( 4 ,  2 ) ;  //
+aElPoderDe ( 10 ,  10 ) ;  //
+10  **  10 ;  //
